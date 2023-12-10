909db901786192d0427e9e715596bddb9f4481a7
der_frühling <der_fruhling@outlook.com>
2023-12-09 21:29:16 -0800

    refurbish day 5 pt. 2 with clap to disable printing

--- src/bin/day5/day5-2.rs
+++ src/bin/day5/day5-2.rs
@@ -1,14 +1,25 @@
-use std::{env, sync::Arc, io::{self, Write}};
+use std::{env, sync::Arc, io::{self, Write}, path::PathBuf};
+
+use clap::Parser;
 
 use crate::common::ConversionMapper;
 
 mod common;
 
-fn main() {
-    let args: Vec<_> = env::args().collect();
-    let file_name = &args[1];
+#[derive(clap::Parser)]
+struct Cli {
+    /// Disables printing debug messages while processing.
+    ///
+    /// May be useful for benchmarking the implementation.
+    #[arg(long)]
+    no_printing: bool,
 
-    let bytes = std::fs::read(file_name)
+    file_path: PathBuf
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let bytes = std::fs::read(cli.file_path)
         .expect("Failed to read file!");
 
     let string = String::from_utf8(bytes).expect("Invalid file contents! Not UTF-8?");
@@ -17,29 +28,39 @@ fn main() {
     let seed_ranges = mapper.seeds.chunks_exact(2).map(|v| (v[0], v[1]));
     let mut smallest = i64::MAX;
     
-    println!("Beginning magic");
+    if !cli.no_printing {
+        println!("Beginning magic");
+    }
     
     let mut total_iterations = 0u128;
     
     for (start, count) in seed_ranges {
-        println!("\x1b[0G\x1b[2KStarting range {}..{}", start, count);
-        print!("Current smallest: {} (iteration: 0/{})", smallest, count);
+        if !cli.no_printing {
+            println!("\x1b[0G\x1b[2KStarting range {}..{}", start, count);
+            print!("Current smallest: {} (iteration: 0/{})", smallest, count);
+        }
         
         for i in start..(start + count) {
             let result = mapper.lookup(i);
             
             if result < smallest {
-                println!("\x1b[0G\x1b[2K{} produced {} (less than {})", i, result, smallest);
-                print!("Current smallest: {}", smallest);
+                if !cli.no_printing {
+                    println!("\x1b[0G\x1b[2K{} produced {} (less than {})", i, result, smallest);
+                    print!("Current smallest: {}", smallest);
+                }
                 smallest = result;
-            } else if total_iterations % (1 << 18) == 0 {
+            } else if total_iterations % (1 << 18) == 0 && !cli.no_printing {
                 print!("\x1b[0G\x1b[2KCurrent smallest: {} (iteration: {}/{})", smallest, i - start, count);
                 io::stdout().flush().expect("No stdout to flush!");
+            } else if !cli.no_printing {
+                total_iterations += 1;
             }
-            
-            total_iterations += 1;
         }
     }
     
-    println!("\x1b[0G\x1b[2KResult: {}", smallest)
+    if cli.no_printing {
+        println!("{}", smallest);
+    } else {
+        println!("\x1b[0G\x1b[2KResult: {}", smallest);
+    }
 }
